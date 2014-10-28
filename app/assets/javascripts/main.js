d706ff5d9f969ed92ffbdc57aa84ab00fccc90bc
alienk703 <alienk703@gmail.com>
2014-10-28 15:03:50 -0400

    json rendered

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,11 +6,11 @@ $(function() {
   $( ".datepicker" ).datepicker({dateFormat: "yy-mm-dd"});
 });
 
-
 // var Finder = {
 
 function flightSearch(){
 
+ 
   var self = this;
     $("form").on('submit', function(event){
       event.preventDefault();
@@ -28,18 +28,19 @@ function flightSearch(){
      var arrdate = $(".datepicker").val();
      var passenger = $(".passenger").val();
      var budget = $("#budget").val();
+     var airportArray = ['BHM','ANC','PHX','LIT','LAX','SFO','DEN','BDL','MIA','MCO','TPA','ATL','HNL','BOI','ORD','IND','DSM','ICT','SDF','MSY','PWM','BWI','BOS','DTW','MSP','JAN','STL','BZN','OMA','LAS','MHT','EWR','ABQ','JFK','LGA','CLT','RDU','FAR','CLE','CMH','OKC','TUL','PDX','PHL','PIT','PVD','CHS','CAE','GSP','MYR','FSD','MEM','BNA','AUS','DAL','ELP','IAH','HOU','SAT','SLC','BTV','IAD','DCA','SEA','CRW','MKE','JAC','SJU']
+     var airport = airportArray[Math.floor(Math.random()*airportArray.length)];
 
     self = this
 
     var htmlString = "<p>Results: </p><ul>";
     $.ajax({
-      url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?fields=trips&key=AIzaSyCtvfwSNDdpQS3PCIoLnJRVcQsMDOGXMa0',
+      url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?fields=trips&key=AIzaSyBFA3QnRcB-HKcCnCA_3dEjtAYln3zGXBU',
       method: 'POST',
       dataType: 'json',
       contentType: 'application/json',
       data: JSON.stringify(
         { 'request': {
-          'maxPrice': budget.to_s,
           'passengers': {
             "kind": "qpxexpress#passengerCounts",
             "adultCount": passenger
@@ -48,44 +49,61 @@ function flightSearch(){
             {
               "kind": "qpxexpress#sliceInput",
               "origin": departure,
-              "destination": arrival,
+              "destination": airport,
               "date": depdate,
               "maxStops": 0
             },
             {
               "kind": "qpxexpress#sliceInput",
-              "origin": arrival,
+              "origin": airport,
               "destination": departure,
               "date": arrdate,
               "maxStops": 0,
             }
           ],
           'maxPrice': budget.to_s,
-          'solutions': 50
+          'solutions': 100
           }
         }
       ),
       success: function(data){
+        var self = this;
         console.log(data);
         console.log("something");
+
+        for (var i = 0; i < data.trips.data.carrier.length; i ++)
+        for (var x = 0; x < data.trips.tripOption.length; x ++){
+
+          var destin = data.trips.data.city[0].name;
+          var carrier = data.trips.data.carrier[i].name;
+          var price = data.trips.tripOption[x].saleTotal;
+
+          htmlString += '<li>' + destin + ':' + carrier + ":"  + price + "<li>";
+          htmlString += "</ul>";
+          self.render(htmlString);
+        }
       },
 
-      // clearData: function(){
-      //   $(".results").html("");
+        render: function(htmlString){
+          this.clearResults();
+
+          $(".results").append(htmlString)
+
+        },
+
+        clearResults: function(){
+          $(".results").html("");
+        },
+      
       // },
 
       failure: function(){
         console.log("Fuck me :(");
-      },
+      }
+
     });
   });
 }
-
-
-   function render(htmlString){
-        this.clearResults();
-        $(".results").append(htmlString)
-      }
 
 
 $(document).ready(function(){
